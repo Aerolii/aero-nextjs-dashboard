6773eca65561d433b4904d28a5b580e6ac43ac47
张晓波 <xiaobo.z@kusaauto.com>
2025-04-28 16:04:20 +0800

    feat: 新增仪表板加载组件和页面，优化卡片和收入图表的异步数据获取

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,11 @@
+// import Dashboard from "@/app/ui/skeletons";
+
+import DashboardSkeleton from "@/app/ui/skeletons";
+
+export default function Loading() {
+  return (
+    <div>
+      <DashboardSkeleton />
+    </div>
+  );
+}
